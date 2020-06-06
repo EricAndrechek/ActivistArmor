9d28ad4b1d6f3a5421ee9915bb30a48ee447d937
Alex Ruan <alexruan2222@gmail.com>
2020-06-06 05:36:34 -0400

    readded expo app

---
+++ frontend/ActivistArmorExpo/App.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import {createStackNavigator} from 'react-navigation-stack';
+
+import FeedScreen from './screens/FeedScreen';
+import MapScreen from './screens/MapScreen';
+import PostScreen from './screens/postscreens/postscreen';
+
+const BottomTab = createBottomTabNavigator();
+
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+
+const initialState = {
+  counter : 0
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    case 'INCREASE_COUNTER':
+      return{counter:state.counter+1}
+  }
+  return state
+}
+
+const store = createStore(reducer);
+
+
+export default function App() {
+  return (
+    <Provider store = {store}>
+      <View style={styles.container}>
+        <NavigationContainer>
+          <BottomTab.Navigator initialRouteName={"Feed"}>
+            <BottomTab.Screen
+              name="Feed"
+              component={FeedScreen}
+              options={{
+                title: 'Feed',
+              }}
+            />
+            <BottomTab.Screen
+              name="Map"
+              component={MapScreen}
+              options={{
+                title: 'Map',
+              }}
+            />
+            <BottomTab.Screen
+              name="Post"
+              component={PostScreen}
+            />
+          </BottomTab.Navigator>
+        </NavigationContainer>
+      </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ frontend/ActivistArmorExpo/screens/postscreens/capturescreen.js
@@ -0,0 +1,200 @@
+
+import * as React from 'react';
+import {ImageBackground, Text, TouchableOpacity, Image, View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import {Header, Card} from 'react-native-elements';
+import {connect} from 'react-redux';
+//import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+export default class CaptureScreen extends React.Component {
+  state = {
+    image: null,
+  };
+
+  render() {
+    let { image } = this.state;
+
+    return (
+      <View style={{flex: 1}}>
+          <Header 
+              backgroundColor='#fff'
+              leftComponent={{icon: 'menu', color: 'black'}}
+              centerComponent={{ text: 'Capture', style: styles.headerText}}
+              //rightComponent={{ text: 'Right component', style: styles.headerText }}
+              containerStyle={{
+                elevation: 10,
+                shadowOffset:{  width: 10,  height: 10,  },
+                shadowColor: 'black',
+                shadowOpacity: 1.0,
+                backgroundColor: 'transparent',
+                paddingBottom: 20,
+                height: 60,
+              }}
+              >
+          </Header>
+        <View style={{ flex: 1}}>
+
+          <TouchableOpacity onPress={this._pickImage} style={styles.infoCard}>
+            <Text style={styles.infoCardText}>Pick an image from camera roll</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={this._takePhoto} style={styles.infoCard}>
+            <Text style={styles.infoCardText}>Take photo2</Text>
+          </TouchableOpacity>
+
+          <View>
+            {
+              image ?
+              <View>
+                <View style={styles.imageContainer}>
+                  <Image source={{ uri: `data:${"image"};base64,${image}` }} style={styles.foodRecommendation} />
+                </View>  
+              </View>
+              : 
+              <View style={styles.imageContainer}>
+                  <Image source={
+                    require('../assets/images/nophoto.jpg')
+                  } style={styles.foodRecommendation} />
+                
+              </View>
+            }  
+          </View>
+          <TouchableOpacity onPress={this._submitPhoto} style={styles.infoCard}>
+            <Text style={styles.infoCardText}> Submit photo </Text>
+          </TouchableOpacity> 
+        </View>
+      </View>
+    );
+  }
+
+  componentDidMount() {
+    this.getPermissionAsync();
+  }
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+      
+      const { status2 } = await Permissions.askAsync(Permissions.CAMERA);
+      if (status2 !== 'granted') {
+        alert('Sorry, we need camera permissions to make this work!');
+      }
+    }
+  };
+
+  _pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+        base64: true,
+      });
+      if (!result.cancelled) {
+        this.setState({ image: result.base64 });
+      }
+    } catch (E) {
+      console.log(E);
+    }
+  };
+
+  _takePhoto = async () => {
+    try {
+      let result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+        base64: true,
+      });
+      if (!result.cancelled) {
+        this.setState({ image: result.base64 });
+      }
+    } catch (E) {
+      console.log(E);
+    }
+  };
+
+  _submitPhoto = () => {
+    //console.log(this.state.image)
+    this.props.navigation.navigate('Analyze', {image: this.state.image})
+  }
+}
+
+
+function mapStateToProps(state){
+  return{
+    counter:state.counter
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return{
+    increaseCounter: () => dispatch({type:'INCREASE_COUNTER'}),
+    decreaseCounter: () => dispatch({type: 'DECREASE_COUNTER'}),
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+  headerBack:{ 
+    height: 60,
+  },
+  imageContainer: {
+    backgroundColor: '#fff',
+    padding: 5,
+    borderRadius: 20,
+    marginHorizontal: 63,
+    margin: 5,
+  },
+  foodRecommendation: {
+    alignSelf: 'center',
+    width: 250,
+    height: 200,
+    borderRadius: 20,
+  },
+  pictureTaking:{
+    alignSelf: 'center',
+    width: 390,
+    height: 200,
+    borderRadius: 10,
+    marginHorizontal: 2,
+    marginBottom: 2,
+  },
+  headerText: {
+    fontSize: 25,
+    color: 'whitesmoke',
+    fontWeight: 'bold',
+  },
+  button: {
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 5,
+    backgroundColor: 'lightblue',
+  },
+  infoCard: {
+    backgroundColor: '#fff',
+    marginHorizontal: 5,
+    margin: 5,
+    padding: 12,
+    borderRadius: 5,
+    elevation: 5,
+    shadowOffset:{  width: 10,  height: 10,  },
+    shadowColor: 'black',
+    shadowOpacity: 0.1,
+    opacity: 0.75,
+  },
+  infoCardText: {
+    textAlign: 'center',
+    fontSize: 18,
+  },
+})
